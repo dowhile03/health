42e7915799f994ee156efe20c2573a9ec8ffb627
Vipul <dowhile03@gmail.com>
2021-05-08 14:31:54 +0530

    vipul's work

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 
-
 const express = require('express');
 const exphbs = require("express-handlebars");
-
+const fetch = require('node-fetch')
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express();
 const port = 3000 || process.env.PORT;
-
+app.use(express.static('public'))
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json());
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 
 app.set('view engine', 'hbs')
 app.use('/',require('./routes/home'))
 app.use('/',require('./routes/blood'))
+app.use('/',require('./routes/daignosis'))
+
 app.listen(port)
